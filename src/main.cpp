dd136de0ab22d382b4e3f881bdcf354d9a3ca2c1
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2026-04-27 12:14:29 +0200

    rgb ledstrip on ESP32S3 board

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2017 Linaro Limited
- * Copyright (c) 2018 Intel Corporation
- * Copyright (c) 2024 TOKITA Hiroshi
- *
- * SPDX-License-Identifier: Apache-2.0
- */
 
 #include <errno.h>
 #include <string.h>
@@ -18,57 +11,50 @@ LOG_MODULE_REGISTER(main);
 #include <zephyr/device.h>
 #include <zephyr/drivers/spi.h>
 #include <zephyr/sys/util.h>
+#include <rgb.hpp>
 
-#define STRIP_NODE		DT_ALIAS(led_strip)
-
+#define STRIP_NODE DT_ALIAS(led_strip)
 #if DT_NODE_HAS_PROP(DT_ALIAS(led_strip), chain_length)
-#define STRIP_NUM_PIXELS	DT_PROP(DT_ALIAS(led_strip), chain_length)
+#define STRIP_NUM_PIXELS DT_PROP(DT_ALIAS(led_strip), chain_length)
 #else
 #error Unable to determine length of LED strip
 #endif
+#define DELAY_TIME      K_MSEC(CONFIG_SAMPLE_LED_UPDATE_DELAY)
+#define RGB(_r, _g, _b) {.r = (_r), .g = (_g), .b = (_b)}
 
-#define DELAY_TIME K_MSEC(CONFIG_SAMPLE_LED_UPDATE_DELAY)
+// static const struct led_rgb colors[] = {
+// 	RGB(CONFIG_SAMPLE_LED_BRIGHTNESS, 0x00, 0x00), /* red */
+// 	RGB(0x00, CONFIG_SAMPLE_LED_BRIGHTNESS, 0x00), /* green */
+// 	RGB(0x00, 0x00, CONFIG_SAMPLE_LED_BRIGHTNESS), /* blue */
+// };
 
-#define RGB(_r, _g, _b) { .r = (_r), .g = (_g), .b = (_b) }
+static struct led_rgb ledStripPixels[STRIP_NUM_PIXELS];
+static const struct device *const ledStripDevice = DEVICE_DT_GET(STRIP_NODE);
 
-static const struct led_rgb colors[] = {
-	RGB(CONFIG_SAMPLE_LED_BRIGHTNESS, 0x00, 0x00), /* red */
-	RGB(0x00, CONFIG_SAMPLE_LED_BRIGHTNESS, 0x00), /* green */
-	RGB(0x00, 0x00, CONFIG_SAMPLE_LED_BRIGHTNESS), /* blue */
-};
+int main(void) {
+    int hueIndex = 0;
+    int hueStep  = 16;
 
-static struct led_rgb pixels[STRIP_NUM_PIXELS];
+    if (!device_is_ready(ledStripDevice)) {
+        LOG_ERR("LED strip device %s is not ready", ledStripDevice->name);
+        return -ENODEV;
+    }
 
-static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
+    while (true) {
+        for (size_t cursor = 0; cursor < STRIP_NUM_PIXELS; cursor++) {
+            uint32_t tmpPixelColor   = rgb::fromHSV((hueIndex + cursor) % 256);
+            ledStripPixels[cursor].r = rgb::red(tmpPixelColor) >> 2;
+            ledStripPixels[cursor].g = rgb::green(tmpPixelColor) >> 2;
+            ledStripPixels[cursor].b = rgb::blue(tmpPixelColor) >> 2;
 
-int main(void)
-{
-	size_t color = 0;
-	int rc;
+            int rc = led_strip_update_rgb(ledStripDevice, ledStripPixels, STRIP_NUM_PIXELS);
+            if (rc) {
+                LOG_ERR("couldn't update strip: %d", rc);
+            }
 
-	if (device_is_ready(strip)) {
-		LOG_INF("Found LED strip device %s", strip->name);
-	} else {
-		LOG_ERR("LED strip device %s is not ready", strip->name);
-		return 0;
-	}
-
-	LOG_INF("Displaying pattern on strip");
-	while (1) {
-		for (size_t cursor = 0; cursor < ARRAY_SIZE(pixels); cursor++) {
-			memset(&pixels, 0x00, sizeof(pixels));
-			memcpy(&pixels[cursor], &colors[color], sizeof(struct led_rgb));
-
-			rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
-			if (rc) {
-				LOG_ERR("couldn't update strip: %d", rc);
-			}
-
-			k_sleep(DELAY_TIME);
-		}
-
-		color = (color + 1) % ARRAY_SIZE(colors);
-	}
-
-	return 0;
+            k_sleep(DELAY_TIME);
+        }
+        hueIndex = (hueIndex + hueStep) % 256;
+    }
+    return 0;
 }
